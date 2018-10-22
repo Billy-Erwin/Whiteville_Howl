c4f534c0cf25d63531a2e47367f9472d54639dec
Billy <Billy.Erwin@ncino.com>
2018-10-22 15:47:54 -0400

    - use col class

--- Howl-Soccer/src/app/sidebar.component.ts
+++ Howl-Soccer/src/app/sidebar.component.ts
@@ -3,15 +3,16 @@ import {Component} from "@angular/core";
 @Component({
 	styleUrls: ['howl.style.css'],
 	selector: 'howlSidebar',
-	template:`<div class="list-group theSide">
-	   <a href="#" class="list-group-item list-group-item-action active">
-		  About Us
-	   </a>
-	   <a href="#" class="list-group-item list-group-item-action">Our Mission</a>
-	   <a href="#" class="list-group-item list-group-item-action">Who We Are</a>
-	   <a href="#" class="list-group-item list-group-item-action">Current Staff</a>
-	   <a href="#" class="list-group-item list-group-item-action">Contact Us</a>
-   </div>`
+	template:`
+		<div class="col-3 list-group theSide">
+			<a href="#" class="list-group-item list-group-item-action active">
+				About Us
+			</a>
+			<a href="#" class="list-group-item list-group-item-action">Our Mission</a>
+			<a href="#" class="list-group-item list-group-item-action">Who We Are</a>
+			<a href="#" class="list-group-item list-group-item-action">Current Staff</a>
+			<a href="#" class="list-group-item list-group-item-action">Contact Us</a>
+		</div>`
 })
 
 export class SidebarComponent{}
